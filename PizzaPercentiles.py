2cde0a705a3845039a675aab94ee75a863b44433
HenshawAnalysis <90053827+HenshawAnalysis@users.noreply.github.com>
2023-02-24 16:27:47 +0000

    Add files via upload

--- PizzaPercentiles.py
+++ PizzaPercentiles.py
@@ -538,7 +538,7 @@ fig, ax = baker.make_pizza(
 
 # add title
 fig.text(
-    0.515, 0.975, "%s (%i, %i mins) - %s" %(player, page, minplay, team),
+    0.515, 0.975, "%s (%i / %i mins) - %s" %(player, page, minplay, team),
     size=16, fontweight='bold', ha="center", color="#000000"
 )
 
@@ -1179,8 +1179,8 @@ if template == 'Forward':
     fig_text(0.91,0.29,"DEFENDING", fontweight='bold',size=16, color="black", alpha=0.5, zorder=2, rotation=270.)
 
     
-fig_text(-0.16,0.96,"%s (%i) - %s" %(player, page, team),  fontweight='bold',size=36)
-fig_text(-0.16,0.91,"%s | %s %s | %i mins | %s template" %(team, league, ssn_, minplay, template), fontweight='bold',size=16)
+fig_text(-0.16,0.96,"%s (%i / %i mins) - %s" %(player, page, minplay, team),  fontweight='bold',size=36)
+fig_text(-0.16,0.91,"%s | %s %s | %s template" %(team, league, ssn_, template), fontweight='bold',size=16)
 fig_text(0.432,0.865,"Average "  + league +  " " + template, fontweight='bold',size=11, fontstyle='italic', color="black", alpha=0.7, zorder=2)
 
 
